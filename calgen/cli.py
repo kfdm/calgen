85346214384a3248ea7e5d90a9eb9a3370d6095f
Paul Traylor <kungfudiscomonkey@gmail.com>
2015-01-06 20:18:08 +0900

    Support multiple input files

--- calgen/cli.py
+++ calgen/cli.py
@@ -7,7 +7,7 @@ logging.basicConfig(level=logging.DEBUG)
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('calendar', help='Calendar config')
+    parser.add_argument('calendar', nargs='+', help='Calendar config')
     args = parser.parse_args()
     cal = calgen.Calendar(args.calendar)
     print cal.format()
